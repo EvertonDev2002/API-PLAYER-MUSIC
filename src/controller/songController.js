3c0f92e63f5ed6881e4c3c6b9b6f2df7e64d1549
EvertonDev2002 <foreverplayer95@gmail.com>
2021-12-31 14:48:52 -0300

    validaçao na listagem de uma musica e verificaçao de qual dado nao existe no banco

--- src/controller/songController.js
+++ src/controller/songController.js
@@ -1,10 +1,10 @@
 const knex = require("../database");
 module.exports = {
   async index(req, res) {
-    const { page } = req.query;
-    if (page) {
+    const { id } = req.query;
+    if (id) {
       results = await knex("tb_song")
-        .where("id_song", "=", page)
+        .where("id_song", "=", id)
         .join("tb_album", "tb_album.id_album", "tb_song.fk_album")
         .join("tb_artist", "tb_artist.id_artist", "tb_song.fk_artist")
         .join("tb_genre", "tb_genre.id_genre", "tb_song.fk_genre")
@@ -22,16 +22,20 @@ module.exports = {
           "title_song",
           "duration"
         );
-      const { fk_album } = results[0];
-      const [count] = await knex("tb_song")
-        .where("fk_album", "=", fk_album)
-        .join("tb_album", "tb_album.id_album", "tb_song.fk_album")
-        .join("tb_artist", "tb_artist.id_artist", "tb_song.fk_artist")
-        .join("tb_genre", "tb_genre.id_genre", "tb_song.fk_genre")
-        .count();
+      if (results.length != 0) {
+        const { fk_album } = results[0];
+        const [count] = await knex("tb_song")
+          .where("fk_album", "=", fk_album)
+          .join("tb_album", "tb_album.id_album", "tb_song.fk_album")
+          .join("tb_artist", "tb_artist.id_artist", "tb_song.fk_artist")
+          .join("tb_genre", "tb_genre.id_genre", "tb_song.fk_genre")
+          .count();
 
-      res.header("X-Total-Count", count["count"]);
-      return res.json(results);
+        res.header("X-Total-Count", count["count"]);
+        return res.json(results);
+      } else {
+        return res.json("Nenhum dado encontrado!");
+      }
     } else {
       const results = await knex("tb_song")
         .join("tb_album", "tb_album.id_album", "tb_song.fk_album")
@@ -90,9 +94,9 @@ module.exports = {
         .limit(1);
 
       if (
-        id_album.length == 0 &&
-        id_artist.length == 0 &&
-        id_genre.length == 0
+        id_album.length === 0 &&
+        id_artist.length === 0 &&
+        id_genre.length === 0
       ) {
         await knex("tb_album").insert({ title_album, albumcover });
         await knex("tb_artist").insert({ artist });
@@ -113,14 +117,96 @@ module.exports = {
           .orderBy("id_genre", "desc")
           .limit(1);
 
-        const fk_album = parseInt(id_album);
-        const fk_artist = parseInt(id_artist);
-        const fk_genre = parseInt(id_genre);
+        await knex("tb_song").insert({
+          fk_album: id_album,
+          fk_artist: id_artist,
+          fk_genre: id_genre,
+          lyrics,
+          file,
+          title_song,
+          duration,
+        });
+
+        return res.status(201).send();
+      } else if (id_album.length === 0) {
+        await knex("tb_album").insert({ title_album, albumcover });
+
+        const [{ id_album }] = await knex("tb_album")
+          .select("id_album")
+          .orderBy("id_album", "desc")
+          .limit(1);
+
+        const [{ id_artist }] = await knex("tb_artist")
+          .select("id_artist")
+          .orderBy("id_artist", "desc")
+          .limit(1);
+
+        const [{ id_genre }] = await knex("tb_genre")
+          .select("id_genre")
+          .orderBy("id_genre", "desc")
+          .limit(1);
 
         await knex("tb_song").insert({
-          fk_album,
-          fk_artist,
-          fk_genre,
+          fk_album: id_album,
+          fk_artist: id_artist,
+          fk_genre: id_genre,
+          lyrics,
+          file,
+          title_song,
+          duration,
+        });
+
+        return res.status(201).send();
+      } else if (id_artist.length === 0) {
+        await knex("tb_artist").insert({ artist });
+
+        const [{ id_album }] = await knex("tb_album")
+          .select("id_album")
+          .orderBy("id_album", "desc")
+          .limit(1);
+
+        const [{ id_artist }] = await knex("tb_artist")
+          .select("id_artist")
+          .orderBy("id_artist", "desc")
+          .limit(1);
+
+        const [{ id_genre }] = await knex("tb_genre")
+          .select("id_genre")
+          .orderBy("id_genre", "desc")
+          .limit(1);
+
+        await knex("tb_song").insert({
+          fk_album: id_album,
+          fk_artist: id_artist,
+          fk_genre: id_genre,
+          lyrics,
+          file,
+          title_song,
+          duration,
+        });
+        return res.status(201).send();
+      } else if (id_genre.length === 0) {
+        await knex("tb_genre").insert({ genre });
+
+        const [{ id_album }] = await knex("tb_album")
+          .select("id_album")
+          .orderBy("id_album", "desc")
+          .limit(1);
+
+        const [{ id_artist }] = await knex("tb_artist")
+          .select("id_artist")
+          .orderBy("id_artist", "desc")
+          .limit(1);
+
+        const [{ id_genre }] = await knex("tb_genre")
+          .select("id_genre")
+          .orderBy("id_genre", "desc")
+          .limit(1);
+
+        await knex("tb_song").insert({
+          fk_album: id_album,
+          fk_artist: id_artist,
+          fk_genre: id_genre,
           lyrics,
           file,
           title_song,
